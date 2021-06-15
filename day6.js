27aafd4acd60e62abedc4719c747fcaf0a652990
himanshucyborg <73119921+himanshucyborg@users.noreply.github.com>
2021-06-16 01:10:23 +0530

    first commit

---
+++ day6.js
@@ -0,0 +1,20 @@
+//1st question
+// function is_Array(arr) {
+//     return Array.isArray(arr);
+// }
+// console.log(is_Array([1,2,4,0]));
+
+//2nd question
+// function array_Clone(arr) {
+//     var cloneArr = arr.slice(); //or cloneArr[] = [...arr];
+//     return cloneArr
+// }
+// console.log(array_Clone([1, 2, [4, 0]]));
+
+//3rd question
+// function first(arr) {
+//     var n = arr[0];
+const newArr = function(arr) {return n }
+//     return n
+// }
+console.log(function([1, 2, [4, 0]]));
